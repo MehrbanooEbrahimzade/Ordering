6be30b3d9a2c38721d4cc4636601aa47d28372a1
mehrbanoo ebrahimzade <mehrbanooebrahimzade@gmail.com>
2023-01-30 16:31:53 +0330

    Add SMS.Api

---
+++ Sms.Api/Consumer/OperationFinishedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using EventBus.Messages.Events;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Sms.Api.Consumer
+{
+    public class OperationFinishedConsumer : IConsumer<OperationFinishedEvent>
+    {
+        private readonly ILogger<OperationFinishedConsumer> _logger;
+
+        public OperationFinishedConsumer(ILogger<OperationFinishedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<OperationFinishedEvent> context)
+        {
+            _logger.LogInformation("dear customer with number :{PhoneNumber}, your order status is --DONE--", context.Message.Number);
+            return Task.CompletedTask;
+        }
+    }
+
+}
---
+++ Sms.Api/Consumer/OrderSavedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using EventBus.Messages.Events;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Sms.Api.Consumer
+{
+    public class OrderSavedConsumer : IConsumer<OrderSavedEvent>
+    {
+        private readonly ILogger<OrderSavedConsumer> _logger;
+
+        public OrderSavedConsumer(ILogger<OrderSavedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<OrderSavedEvent> context)
+        {
+            _logger.LogInformation("hello dear {UserName}, your order saved! ", context.Message.UserName);
+            return Task.CompletedTask;
+        }
+    }
+}
